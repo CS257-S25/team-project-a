f390f7f091892665f3c7f603d78e4a085f66d5eb
Digicat509 <alexandersnyder@yahoo.com>
2025-04-20 20:20:18 -0500

    Added basic command line input parsing and method calls

---
+++ ProductionCode/data_procesor.py
@@ -0,0 +1,8 @@
+def meeting_frequency():
+    pass
+
+def meeting_count():
+    pass
+
+def drug_sale_arrests(lower, upper):
+    pass
--- cl.py
+++ cl.py
@@ -1,4 +1,48 @@
-'''
-The eventual location for the command line interface (CLI) for the project.
-This will be the entry point for the project when run from the command line.
-'''
+"""Code for the Command Line Interface"""
+
+import sys
+from ProductionCode import data_procesor
+
+def main():
+    """Runs the Program"""
+    process_input()
+
+def process_input():
+    """Takes in the command line input and calls
+    the methods from production code to get the requested info"""
+    if len(sys.argv) == 0:
+        print_usage_statement()
+    elif len(sys.argv) > 1:
+        if sys.argv[1] == "--meeting":
+            if len(sys.argv) == 3:
+                if sys.argv[2] == "frequency" or sys.argv[2] == "freq":
+                    data_procesor.meeting_frequency()
+                if sys.argv[2] == "count":
+                    data_procesor.meeting_count()
+            else:
+                print_usage_statement()
+        elif sys.argv[1] == "--sellArrest":
+            if get_sys_argv_length() == 4:
+                try:
+                    data_procesor.drug_sale_arrests(int(sys.argv[2]), int(sys.argv[3]))
+                except ValueError:
+                    print_usage_statement()
+            else:
+                print_usage_statement()
+        else:
+            print_usage_statement()
+    else:
+        print_usage_statement()
+
+def get_sys_argv_length():
+    """gives the numebr of commands in the comand line input"""
+    return len(sys.argv)
+
+def print_usage_statement():
+    """Prints the class usage statement when improper input is given"""
+    print("Usage:" \
+    "\npython3 --meeting [\"frequency\", \"count\"]" \
+    "\npython3 --sellArrests lowerBoundCount UpperBoundCount")
+
+if __name__=="__main__":
+    main()
