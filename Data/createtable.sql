b52be2417d32384ac4cb973e7e246d7b30d6f561
Starchaser158 <nicholastheus922@gmail.com>
2025-05-12 15:48:38 -0400

    New smallData and createtable files

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,5 +1,11 @@
 DROP TABLE IF EXISTS drug_data;
 CREATE TABLE drug_data (
+    CID int,
     NSHLPM int,
-    ARSTDRG int
+    ARSTDRG int,
+    ALCPRB1 int,
+    ALCPRB3 int,
+    DRGPRB1 int,
+    DRGPRB3 int,
+
 );
