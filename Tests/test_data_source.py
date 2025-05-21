968f2b7f70745ed32c811577a0b8546c5e60028b
Digicat509 <alexandersnyder@yahoo.com>
2025-05-21 19:06:12 -0400

    test coverage work

--- Tests/test_data_source.py
+++ Tests/test_data_source.py
@@ -2,7 +2,7 @@
 
 import unittest
 from unittest.mock import MagicMock, patch
-from ProductionCode.datasource import DataSource
+from ProductionCode.datasource import DataSource, process_graph_data
 
 
 class TestGetMeetingsAttended(unittest.TestCase):
@@ -48,3 +48,33 @@ class TestGetArrestRanges(unittest.TestCase):
             data_soucre.get_arrest_ranges(1, 10),
             283
         )
+
+class TestGetGraphData(unittest.TestCase):
+    """Tests the path based method calls and pages"""
+
+    def setUp(self):
+        #create a mock connection and cursor
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+
+    @patch('ProductionCode.datasource.psycopg2.connect')
+    def test_route(self, mock_connect):
+        """Tests a correct path that should display the methods result"""
+        mock_connect.return_value = self.mock_conn
+		#set what it should return
+        self.mock_cursor.fetchall.return_value = (
+            [[1]]*2
+            )
+        data_soucre = DataSource()
+        self.assertEqual(
+            data_soucre.get_graph_data(),
+            [[1, 1], [1, 1]]
+        )
+
+class TestProcessGraphData(unittest.TestCase):
+    """Tests the graph data combining function"""
+
+    def test_list_combine(self):
+        """Tests that the two list are combiend correctly"""
+        self.assertEqual(process_graph_data([[1], [2], [3]], [[0], [0], [0]]),
+                         [[1, 0], [2, 0], [3, 0]])
