6f91ca87f01c6b37845d8af39671b40bf0cffdd8
Digicat509 <alexandersnyder@yahoo.com>
2025-05-21 21:03:47 -0400

    test app work

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -2,33 +2,28 @@
 
 import unittest
 from unittest.mock import MagicMock, patch
+
 from app import app
 
 
-# class TestMainPage(unittest.TestCase):
-#     """Tests the apps home page"""
+class TestMainPage(unittest.TestCase):
+    """Tests the apps home page"""
 
-#     def __init__(self, methodName="runTest"):
-#         super().__init__(methodName)
-#         self.app = app.test_client()
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = app.test_client()
 
-#     def test_route(self):
-#         """Tests that the home page has the correct welcome text"""
-#         self.app = app.test_client()
-#         response = self.app.get("/", follow_redirects=True)
-#         self.assertEqual(
-#             b"Hello! Welcome to our website with the amazingly"
-#             b"curated title: Analyzing Criminal Drug Abuse Treatment in Females"
-#             b"\nAlso known as drug_abuse_treatment.py"
-#             b"\n"
-#             b"\n"
-#             b"Here are the main directories of our program for your research/interests: "
-#             b"\nFor frequencies or counts "
-#             b"of meeting attendance: (url)/meeting/[frequency], [count]"
-#             b"\nFor drug sale arrests amount: "
-#             b"(url)/drug-sale-arrests/lowerBoundCount/upperBoundCount",
-#             response.data,
-#         )
+    def test_route(self):
+        """Tests that the home page has the correct welcome text"""
+        self.app = app.test_client()
+        response = self.app.get("/", follow_redirects=True)
+        self.assertIn(
+            b'<p>This is a site designed to ' +
+            b'facilitate the analysis of a \n        ' +
+            b'survey on people who have suffered from drug abuse and as a ' +
+            b'result have been arrested.</p>',
+            response.data,
+        )
 
 
 class TestGetMeetingFrequency(unittest.TestCase):
@@ -38,14 +33,24 @@ class TestGetMeetingFrequency(unittest.TestCase):
         super().__init__(methodName)
         self.app = app.test_client()
 
+    def setUp(self):
+        """Sets up the dummy data"""
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+
     # @patch('ProductionCode.datasource.DataSource.get_freq_meetings_attended')
-    # def test_route(self, mock_get_freq_meeting_attended):
+    # def test_route(self, mock_connect):
     #     """Tests a correct path that should display the methods result"""
-    #     mock_get_freq_meeting_attended.return_value = "32.71"
+    #     mock_connect.return_value = self.mock_conn
+    # 	#set what it should return
+    #     self.mock_cursor.fetchall.return_value = (
+    #         [[1]]
+    #         )
     #     self.app = app.test_client()
     #     response = self.app.get("/meeting/frequency", follow_redirects=True)
+    #     print(response.data)
     #     self.assertEqual(
-    #         b"The average percentage of meetings attended is 32.71%", response.data
+    #         b"The average percentage of meetings attended is 100%", response.data
     #     )
 
     def test_bad_route(self):
@@ -56,7 +61,7 @@ class TestGetMeetingFrequency(unittest.TestCase):
             b"404 Not Found: The requested URL was not found on the server. "
             + b"If you entered the URL manually please check your spelling and try again. "
             + b"Sorry, wrong format, do this instead /meeting/frequency or "
-            + b"/meeting/count or arrests/low/high eg. arrests/1/3",
+            + b"/meeting/count or arrests/low/high eg. sellArrest/1/3",
             response.data,
         )
 
@@ -73,15 +78,13 @@ class TestGetMeetingCount(unittest.TestCase):
         self.mock_conn = MagicMock()
         self.mock_cursor = self.mock_conn.cursor.return_value
 
-    @patch('ProductionCode.datasource.psycopg2.connect')
+    @patch("ProductionCode.datasource.psycopg2.connect")
     def test_route(self, mock_connect):
         """Tests a correct path that should display the methods result"""
         self.app = app.test_client()
         mock_connect.return_value = self.mock_conn
-		#set what it should return
-        self.mock_cursor.fetchall.return_value = (
-            [[1.6357466063348416]]
-            )
+        # set what it should return
+        self.mock_cursor.fetchall.return_value = [[1.6357466063348416]]
         response = self.app.get("/meeting/count", follow_redirects=True)
         self.assertEqual(
             b"The average number of self-help meetings attended is 1.64", response.data
@@ -95,63 +98,72 @@ class TestGetMeetingCount(unittest.TestCase):
             b"404 Not Found: The requested URL was not found on the server. "
             + b"If you entered the URL manually please check your spelling and try again. "
             + b"Sorry, wrong format, do this instead /meeting/frequency or "
-            + b"/meeting/count or arrests/low/high eg. arrests/1/3",
+            + b"/meeting/count or arrests/low/high eg. sellArrest/1/3",
             response.data,
         )
 
 
-# class TestGetMeetingPage(unittest.TestCase):
-#     """Tests the HTML meeting page"""
+class TestGetMeetingPage(unittest.TestCase):
+    """Tests the HTML meeting page"""
 
-#     def test_route(self):
-#         """Tests a correct path that should display the methods result"""
-#         self.app = app.test_client()
-#         response = self.app.get("/meeting", follow_redirects=True)
-#         print(response.data)
-#         self.assertEqual(
-#             b"<html>\n\t<head>\n\t\t<title>Self Help Meeting " +
-#             b"Attendance\n\t</title>\n\t</head>\n\t<body>\n\t\t<h1>" +
-#             b"Self Help Meeting Attendance</h1>\n\t\t" +
-#             b"<ul>\n\t\t\t<li>Average meetings attended by " +
-#             b"study participants: {{count}}</li>\n\t\t\t<li>" +
-#             b"Average frequency of meeting attendance " +
-#             b"by study participants: {{freq}}%</li>\n\t\t</ul>\n\t</body>\n</html>",
-#             response.data
-#         )
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = app.test_client()
 
-# class TestDrugSaleArrests(unittest.TestCase):
-#     """Tests the drug sale arrests route"""
+    def setUp(self):
+        """Sets up the dummy data"""
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
 
-#     def __init__(self, methodName="runTest"):
-#         super().__init__(methodName)
-#         self.app = app.test_client()
+    @patch("ProductionCode.datasource.psycopg2.connect")
+    def test_route(self, mock_connect):
+        """Tests a correct path that should display the methods result"""
+        self.app = app.test_client()
+        mock_connect.return_value = self.mock_conn
+        # set what it should return
+        self.mock_cursor.fetchall.return_value = [[1.6357466063348416]]
+        response = self.app.get("/meeting", follow_redirects=True)
+        self.assertIn(
+            b'li>Average meetings attended by study participants: 1.64</li>' +
+            b'\n                <li>Average frequency of meeting attendance by study ' +
+            b'participants: 100.0%</li>',
+            response.data,
+        )
 
-#     def setUp(self):
-#         """Sets up the dummy data"""
-#         self.mock_conn = MagicMock()
-#         self.mock_cursor = self.mock_conn.cursor.return_value
+class TestDrugSaleArrests(unittest.TestCase):
+    """Tests the drug sale arrests route"""
 
-#     @patch('ProductionCode.datasource.psycopg2.connect')
-#     def test_drug_sale(self, mock_connect):
-#         """Test for route for drug sale arrests"""
-#         self.app = app.test_client()
-#         mock_connect.return_value = self.mock_conn
-#         #set what it should return
-#         self.mock_cursor.fetchall.return_value = (
-#             [None]*283
-#             )
-#         response = self.app.get('/sellArrests/1/10', follow_redirects=True)
-#         self.assertEqual(b"The number of people who were arrested between 1 and 10 times is: 283",
-#                           response.data)
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = app.test_client()
 
-    # def test_bad_route(self):
-    #     """Test a bad path that should display a correct usage hint"""
-    #     self.app = app.test_client()
-    #     response = self.app.get("/0", follow_redirects=True)
-    #     self.assertEqual(
-    #         b"404 Not Found: The requested URL was not found on the server. " +
-    #         b"If you entered the URL manually please check your spelling and try again. " +
-    #         + b"Sorry, wrong format, do this instead /meeting/frequency or "
-            # + b"/meeting/count or arrests/low/high eg. arrests/1/3",
-    #         response.data,
-    #     )
+    def setUp(self):
+        """Sets up the dummy data"""
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+
+    @patch('ProductionCode.datasource.psycopg2.connect')
+    def test_drug_sale(self, mock_connect):
+        """Test for route for drug sale arrests"""
+        self.app = app.test_client()
+        mock_connect.return_value = self.mock_conn
+        #set what it should return
+        self.mock_cursor.fetchall.return_value = (
+            [None]*283
+            )
+        response = self.app.get('/sellArrest/lower/upper?lower=1&upper=10', follow_redirects=True)
+        self.assertIn(b'The number of people who were arrested between 1 and 10<br>' +
+                      b'\n        times is: 283',
+                          response.data)
+
+def test_bad_route(self):
+    """Test a bad path that should display a correct usage hint"""
+    self.app = app.test_client()
+    response = self.app.get("/0", follow_redirects=True)
+    self.assertEqual(
+        b"404 Not Found: The requested URL was not found on the server. " +
+        b"If you entered the URL manually please check your spelling and try again. " +
+        b"Sorry, wrong format, do this instead /meeting/frequency or " +
+        b"/meeting/count or arrests/low/high eg. sellArrest/1/3",
+        response.data,
+    )
--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from ProductionCode.datasource import DataSource
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     """Creates a homepage that has user instructions, returns a HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
-    return render_template('home_page.html', pages=pages)
+    return render_template("home_page.html", pages=pages)
     # return 'Hello! Welcome to our website with the amazingly' \
     #     'curated title: Analyzing Criminal Drug Abuse Treatment in Females' \
     #     '\nAlso known as drug_abuse_treatment.py' \
@@ -20,102 +21,119 @@ def homepage():
     #     '\nFor drug sale arrests amount: \
     #      (url)/drug-sale-arrests/lowerBoundCount/upperBoundCount' \
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    """Makes a page for the user when an incorrect url is given 
+    """Makes a page for the user when an incorrect url is given
     takes in an error e and displays it, returns a string"""
-    return str(e)+" Sorry, wrong format, do this instead /meeting/frequency or " \
-        "/meeting/count or arrests/low/high eg. arrests/1/3"
+    return (
+        str(e) + " Sorry, wrong format, do this instead /meeting/frequency or "
+        "/meeting/count or arrests/low/high eg. sellArrest/1/3"
+    )
+
 
 @app.errorhandler(500)
 def python_bug(e):
-    """Makes a page when there is a bug in the underlying python code 
+    """Makes a page when there is a bug in the underlying python code
     takes in an error e and displays it, returns a string"""
-    return "Eek, a bug: "+str(e)
+    return "Eek, a bug: " + str(e)
+
 
 @app.route("/search", methods=["POST", "GET"], strict_slashes=False)
 def display_page_based_on_search():
     """Dynamicaly renders a page based on passed in search parameters,
     returns a HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
-    if request.method == 'POST':
-        response = request.form['search'].replace(" ", "_")
+    if request.method == "POST":
+        response = request.form["search"].replace(" ", "_")
         if response == "meetings":
             data_source = DataSource()
             return render_template(
-                "self_help_meeting_page.html", count=data_source.get_ave_meetings_attended(),
-                freq=data_source.get_freq_meetings_attended(), pages=pages
+                "self_help_meeting_page.html",
+                count=data_source.get_ave_meetings_attended(),
+                freq=data_source.get_freq_meetings_attended(),
+                pages=pages,
             )
         if response == "arrests":
             pages = ["home", "meetings", "data overview", "arrests"]
             data_source = DataSource()
-            return render_template('sell_arrest.html', pages=pages)
+            return render_template("sell_arrest.html", pages=pages)
         if response == "data_overview":
             pages = ["home", "meetings", "data overview", "arrests"]
             data_source = DataSource()
-            return render_template('data_overview_page.html', pages=pages)
+            return render_template("data_overview_page.html", pages=pages)
         if response == "home":
             return render_template("home_page.html", pages=pages)
         return render_template("home_page.html", pages=pages)
     return render_template("home_page.html", pages=pages)
 
-@app.route('/meeting', strict_slashes=False)
+
+@app.route("/meeting", strict_slashes=False)
 def get_meeting():
     """Makes a page that runs when a user request is given for meeting data
     returns an HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
     data_source = DataSource()
-    return render_template('self_help_meeting_page.html',
-                           count=data_source.get_ave_meetings_attended(),
-                           freq = data_source.get_freq_meetings_attended(),
-                           pages=pages
-                           )
+    return render_template(
+        "self_help_meeting_page.html",
+        count=data_source.get_ave_meetings_attended(),
+        freq=data_source.get_freq_meetings_attended(),
+        pages=pages,
+    )
 
-@app.route('/sellArrest', strict_slashes=False)
+
+@app.route("/sellArrest", strict_slashes=False)
 def sell_arrest():
     """Determines the route to the drug arrests page
     which will take in user input, returns a HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
-    return render_template('sell_arrest.html', pages=pages)
+    return render_template("sell_arrest.html", pages=pages)
 
-@app.route('/dataOverview', strict_slashes=False)
+
+@app.route("/dataOverview", strict_slashes=False)
 def get_data_overview():
     """Makes a page that runs when a user request is given for the graphical data
     returns an HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
     data_source = DataSource()
-    return render_template('data_overview_page.html',
-                           pages=pages, data=data_source.get_graph_data())
+    return render_template(
+        "data_overview_page.html", pages=pages, data=data_source.get_graph_data()
+    )
 
-@app.route('/meeting/frequency', strict_slashes=False)
+
+@app.route("/meeting/frequency", strict_slashes=False)
 def get_meeting_freq():
     """Makes a page that runs when a user request is given for meeting data
     returns a string"""
     data_source = DataSource()
     freq = data_source.get_freq_meetings_attended()
-    return "The average percentage of self-help meetings attended is "+str(freq)+"%"
+    return "The average percentage of self-help meetings attended is " + str(freq) + "%"
 
-@app.route('/meeting/count', strict_slashes=False)
+
+@app.route("/meeting/count", strict_slashes=False)
 def get_meeting_count():
     """Makes a page that runs when a user request is given for meeting data
     returns a string"""
     data_source = DataSource()
     count = data_source.get_ave_meetings_attended()
-    return "The average number of self-help meetings attended is "+str(count)
+    return "The average number of self-help meetings attended is " + str(count)
 
-@app.route('/sellArrest/<lower>/<upper>', strict_slashes=False, methods=["POST", "GET"])
+
+@app.route("/sellArrest/<lower>/<upper>", strict_slashes=False, methods=["POST", "GET"])
 def sell_arrest_spec(lower, upper):
     """Makes the data page for the drug sale info
     takes in two integers a low range and a high range,
     returns a HTML page"""
     pages = ["home", "meetings", "data overview", "arrests"]
     data_source = DataSource()
-    lower = int(request.args.get('lower'))
-    upper = int(request.args.get('upper'))
+    lower = int(request.args.get("lower"))
+    upper = int(request.args.get("upper"))
     result = data_source.get_arrest_ranges(lower, upper)
 
-    return render_template('sell_arrest_spec.html', lower=lower, upper=upper,
-                            result=result, pages=pages)
+    return render_template(
+        "sell_arrest_spec.html", lower=lower, upper=upper, result=result, pages=pages
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
