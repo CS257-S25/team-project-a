6942a0ea299f0ab06c7fbe7627073c731abf3e69
Digicat509 <alexandersnyder@yahoo.com>
2025-05-21 21:24:14 -0400

    test improvements

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -25,47 +25,6 @@ class TestMainPage(unittest.TestCase):
             response.data,
         )
 
-
-class TestGetMeetingFrequency(unittest.TestCase):
-    """Tests the path based method calls and pages"""
-
-    def __init__(self, methodName="runTest"):
-        super().__init__(methodName)
-        self.app = app.test_client()
-
-    def setUp(self):
-        """Sets up the dummy data"""
-        self.mock_conn = MagicMock()
-        self.mock_cursor = self.mock_conn.cursor.return_value
-
-    # @patch('ProductionCode.datasource.DataSource.get_freq_meetings_attended')
-    # def test_route(self, mock_connect):
-    #     """Tests a correct path that should display the methods result"""
-    #     mock_connect.return_value = self.mock_conn
-    # 	#set what it should return
-    #     self.mock_cursor.fetchall.return_value = (
-    #         [[1]]
-    #         )
-    #     self.app = app.test_client()
-    #     response = self.app.get("/meeting/frequency", follow_redirects=True)
-    #     print(response.data)
-    #     self.assertEqual(
-    #         b"The average percentage of meetings attended is 100%", response.data
-    #     )
-
-    def test_bad_route(self):
-        """Test a bad path that should display a correct usage hint"""
-        self.app = app.test_client()
-        response = self.app.get("/0", follow_redirects=True)
-        self.assertEqual(
-            b"404 Not Found: The requested URL was not found on the server. "
-            + b"If you entered the URL manually please check your spelling and try again. "
-            + b"Sorry, wrong format, do this instead /meeting/frequency or "
-            + b"/meeting/count or arrests/low/high eg. sellArrest/1/3",
-            response.data,
-        )
-
-
 class TestGetMeetingCount(unittest.TestCase):
     """Tests the path based method calls and pages"""
 
@@ -74,7 +33,7 @@ class TestGetMeetingCount(unittest.TestCase):
         self.app = app.test_client()
 
     def setUp(self):
-        """Sets up the dummy data"""
+        """Sets up the mock database"""
         self.mock_conn = MagicMock()
         self.mock_cursor = self.mock_conn.cursor.return_value
 
@@ -102,7 +61,6 @@ class TestGetMeetingCount(unittest.TestCase):
             response.data,
         )
 
-
 class TestGetMeetingPage(unittest.TestCase):
     """Tests the HTML meeting page"""
 
@@ -111,7 +69,7 @@ class TestGetMeetingPage(unittest.TestCase):
         self.app = app.test_client()
 
     def setUp(self):
-        """Sets up the dummy data"""
+        """Sets up the mock database"""
         self.mock_conn = MagicMock()
         self.mock_cursor = self.mock_conn.cursor.return_value
 
@@ -138,12 +96,21 @@ class TestDrugSaleArrests(unittest.TestCase):
         self.app = app.test_client()
 
     def setUp(self):
-        """Sets up the dummy data"""
+        """Sets up the mock database"""
         self.mock_conn = MagicMock()
         self.mock_cursor = self.mock_conn.cursor.return_value
 
+    def test_drug_sale_main_page(self):
+        """Tests that the home page has the correct welcome text"""
+        self.app = app.test_client()
+        response = self.app.get("/sellArrest", follow_redirects=True)
+        self.assertIn(
+            b'Please enter your lower and upper bounds as directed',
+            response.data,
+        )
+
     @patch('ProductionCode.datasource.psycopg2.connect')
-    def test_drug_sale(self, mock_connect):
+    def test_drug_sale_result_page(self, mock_connect):
         """Test for route for drug sale arrests"""
         self.app = app.test_client()
         mock_connect.return_value = self.mock_conn
@@ -167,3 +134,63 @@ def test_bad_route(self):
         b"/meeting/count or arrests/low/high eg. sellArrest/1/3",
         response.data,
     )
+
+class TestDataOverview(unittest.TestCase):
+    """Tests the HTML data overview page"""
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = app.test_client()
+
+    def setUp(self):
+        """Sets up the mock database"""
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+
+    def test_route(self):
+        """Tests a correct path that should display the methods result"""
+        self.app = app.test_client()
+        response = self.app.get("/dataOverview", follow_redirects=True)
+        self.assertIn(
+            b'<h2 id="exclusive">Data Overview</h2>',
+            response.data,
+        )
+
+
+class TestSearch(unittest.TestCase):
+    """Tests the search page"""
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.app = app.test_client()
+
+    def setUp(self):
+        """Sets up the mock database"""
+        self.mock_conn = MagicMock()
+        self.mock_cursor = self.mock_conn.cursor.return_value
+
+    # @patch("ProductionCode.datasource.psycopg2.connect")
+    # def test_meeting_page(self, mock_connect):
+    #     """Tests a correct path that should display the methods result"""
+    #     self.app = app.test_client()
+    #     mock_connect.return_value = self.mock_conn
+    #     # set what it should return
+    #     self.mock_cursor.fetchall.return_value = [[1.6357466063348416]]
+    #     response = self.app.get("/meeting/count", follow_redirects=True)
+    #     self.assertEqual(
+    #         b"The average number of self-help meetings attended is 1.64", response.data
+    #     )
+
+    # @patch('ProductionCode.datasource.psycopg2.connect')
+    # def test_drug_sale_result_page(self, mock_connect):
+    #     """Test for route for drug sale arrests"""
+    #     self.app = app.test_client()
+    #     mock_connect.return_value = self.mock_conn
+    #     #set what it should return
+    #     self.mock_cursor.fetchall.return_value = (
+    #         [None]*283
+    #         )
+    #     response = self.app.get('/sellArrest/lower/upper?lower=1&upper=10', follow_redirects=True)
+    #     self.assertIn(b'The number of people who were arrested between 1 and 10<br>' +
+    #                   b'\n        times is: 283',
+    #                       response.data)
